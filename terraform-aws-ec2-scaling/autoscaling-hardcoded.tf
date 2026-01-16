7b4a8457e0f8bcfb9ee9f8507738ca27c1256148
raghu.g <raghunandan.g@zohocorp.com>
2026-01-16 18:45:37 +0530

    UPSIZE instances - Should be BLOCKED by guardian
    - Changed t3.micro -> t3.2xlarge (8x upsize!)
    - Changed t3.small -> m5.8xlarge (massive upsize!)
    
    This PR should FAIL the guardian extension check.

--- terraform-aws-ec2-scaling/autoscaling-hardcoded.tf
+++ terraform-aws-ec2-scaling/autoscaling-hardcoded.tf
@@ -36,7 +36,7 @@ resource "aws_iam_instance_profile" "asg_test" {
 resource "aws_launch_template" "test_hardcoded" {
   name_prefix   = "${var.project_name}-test-lt-"
   image_id      = data.aws_ami.amazon_linux_2.id
-  instance_type = "t3.micro"  # HARDCODED - change this to test your parser
+  instance_type = "t3.2xlarge"  # UPSIZED FROM t3.micro - THIS SHOULD BE BLOCKED!
   key_name      = var.key_name
 
   iam_instance_profile {
@@ -85,7 +85,7 @@ resource "aws_launch_template" "test_hardcoded" {
 # HARDCODED INSTANCE TYPE - Another example
 resource "aws_instance" "standalone_test" {
   ami                    = data.aws_ami.amazon_linux_2.id
-  instance_type          = "t3.small"  # HARDCODED - change this to test your parser
+  instance_type          = "m5.8xlarge"  # UPSIZED FROM t3.small - THIS SHOULD BE BLOCKED!
   subnet_id              = aws_subnet.public[0].id
   vpc_security_group_ids = [aws_security_group.asg.id]
   iam_instance_profile   = aws_iam_instance_profile.asg_test.name
